5d7ec8a1c233b396b5861290ef54a4d0d9a4d8da
Sinmi Kareem <sokareem@umich.edu>
2023-07-09 20:19:43 -0400

    Initial Commit

---
+++ CarParts/CarPartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarPartsApp.swift
+//  CarParts
+//
+//  Created by Sinmisola Kareem on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CarPartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
